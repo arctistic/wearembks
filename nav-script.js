60f55b55e6341632d90744003836967a787f031a
akhilRatnaChettri <chettri.akhilratna@gmail.com>
2020-01-17 15:20:29 +0530

    Dropdown disappears when clicked outside

--- nav-script.js
+++ nav-script.js
@@ -4,9 +4,41 @@ $(document).ready(() => {
     $(".dropdown-sandwich").on("click", () => {
         $(".nav-dropdown").toggleClass("toggle-display");
         $(".dropdown-sandwich").toggleClass("rotate-sandwich");
-
     });
 
+    // Collapse side dropdown on click on windows
+    $(window).on("mouseup",function(event){
+        let dropdown =  $(".nav-dropdown")[0];
+        let lists = document.querySelectorAll(".nav-dropdown li");
+        listClicked = false;
+        lists.forEach(function(list){
+            if(list == event.target){
+                listClicked = true;
+            }
+        });
+        let downArrows = document.querySelectorAll(".down-arrow");
+        let downArrowClicked = false;
+        downArrows.forEach(function(downArrow){
+            if(downArrow == event.target){
+                downArrowClicked = true;
+                console.log(downArrowClicked);
+            }
+        });
+
+        if(event.target != dropdown
+            && event.target.parentNode != dropdown
+            && !listClicked
+            && event.target != $(".dropdown-sandwich i")[0]
+            && event.target != $(".fa-chevron-down")[0]
+            && !downArrowClicked
+            && $(".fa-chevron-left")[0] != event.target){
+                console.log(event.target);
+                $(".nav-dropdown").removeClass("toggle-display");
+            $(".dropdown-sandwich").removeClass("rotate-sandwich");
+        }
+        console.log(event.target);
+    });
+    
     
     // Shifts dropdown menu on scroll below orange band
     $(window).on('scroll', () => {
@@ -25,7 +57,6 @@ $(document).ready(() => {
     $(".side-sub-dropdown-chapters").on("click", () => {
         $(".side-sub-dropdown-chapters").toggleClass("nav-sub-dropdown-toggle");
     });
-
 
     // Makes navbar fixed on scroll below itself
     let navPos = $("nav")[0].getBoundingClientRect().y;
