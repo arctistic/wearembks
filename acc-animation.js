d52dafb68db34cd444ca34e7218b204a7fc33b1a
akhilRatnaChettri <chettri.akhilratna@gmail.com>
2020-05-04 03:00:30 +0530

    Setup new style: Accordians for each part of a project, new image card style; Sidebar-slider width problem resolved;

--- acc-animation.js
+++ acc-animation.js
@@ -1,9 +1,11 @@
 $(document).ready( () => {
     
+    let firstAcc =  $(".acc-p").eq(0)[0];
     $(".acc-p").eq(0).height($(".acc-p").eq(0)[0].scrollHeight);
+
     $(".acc-animate .card-h2").on('click', function(){
         let heightToShow = $(this).next()[0].scrollHeight;
-        $(this).parent().children(".acc-p").height(0);
+        // $(this).parent().children(".acc-p").height(0);
         if($(this).next().height() == 0){
             $(this).next().height(heightToShow);
         } else {
