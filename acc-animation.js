0322aaa6655af6d6da6c0b25fd0591b9528dbd7f
akhilRatnaChettri <chettri.akhilratna@gmail.com>
2020-05-15 08:19:01 +0530

    Setup major style changes, about page done

--- acc-animation.js
+++ acc-animation.js
@@ -2,15 +2,31 @@ $(document).ready( () => {
     
     let firstAcc =  $(".acc-p").eq(0)[0];
     $(".acc-p").eq(0).height($(".acc-p").eq(0)[0].scrollHeight);
+    
+    $(".acc-animate .card-h2").eq(0).children('.fa-chevron-down').css({
+        transform: 'rotate(180deg)'
+    })
 
     $(".acc-animate .card-h2").on('click', function(){
         let heightToShow = $(this).next()[0].scrollHeight;
         // $(this).parent().children(".acc-p").height(0);
         if($(this).next().height() == 0){
             $(this).next().height(heightToShow);
+            $(this).children('.fa-chevron-down').css({
+                transform: 'rotate(180deg)',
+                transition: '0.3s all ease-in-out'
+            })
         } else {
             $(this).next().height(0);
+            $(this).children('.fa-chevron-down').css({
+                transform: 'rotate(0deg)',
+                transition: '0.3s all ease-in-out'
+            })
         }
+        
+
+        
+
     });
 
     $(window).on("resize", function(){
